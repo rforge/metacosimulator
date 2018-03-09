046a8bb12560e7e81b5e4a6f17fc031633f1c141
dumoulin <dumoulin@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-03-09 15:06:05 +0000

    fixes according to RcppGSLExample (in rcppgsl/inst/examples)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/metacosimulator@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/test_link.cpp
+++ pkg/src/test_link.cpp
@@ -1,4 +1,6 @@
 // Libraries
+#include <RcppGSL.h>
+
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
@@ -16,51 +18,7 @@
 
 //g++ -O3 -o test_link test_link.cpp -lgsl -lgslcblas -lm 
 
-using namespace std; // peut-être à enlever, je ne sais plus.
-//extern "C" {
-//void metacommunity_model(double *parameters,double *stat_output){
-
-  
-//void gsl_ran_multinomial (const gsl_rng * r, size_t K, unsigned int N, const double p[], unsigned int n[])
-
-// }
-//}
-
-// test 1 - link avec gsl -> OK
-/*int main() {
-  const gsl_rng_type * T;
-  gsl_rng * r;
-  gsl_rng_env_setup();
-
-  T = gsl_rng_default;
-  r = gsl_rng_alloc (T);
-  unsigned long int seed = 2018;
-  gsl_rng_set (r, seed);
-  
-  unsigned int K=4;
-  unsigned int n=10;
-  unsigned int *x;
-  x = new unsigned int[4];
-  for (int i=0;i<4;i++){
-	x[i]=0;
-  }
-  double *p;
-  p = new double[4];
-  for (int i=0;i<4;i++){
-	p[i]=i+0.0;
-  }
-  gsl_ran_multinomial(r,K,n,p,x);
-  for (int i=0;i<4;i++){
-	  cout<<x[i];
-	  cout<<" ";
-  }
-  
-  gsl_rng_free (r);
-  return 0;
-}
-*/
-
-// test 2 - en lien avec R
+using namespace std; 
 extern "C" {
 void metacommunity_model(double *parameters,unsigned int *stats_output){
 
@@ -68,8 +26,7 @@ void metacommunity_model(double *parameters,unsigned int *stats_output){
   gsl_rng * r;
   gsl_rng_env_setup();
 
-  T = gsl_rng_default;
-  r = gsl_rng_alloc (T);
+  r = gsl_rng_alloc (gsl_rng_default);
   unsigned long int seed = 2018;
   gsl_rng_set (r, seed);
   
